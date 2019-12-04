0a3f43b08252762d02d728ec4efa02819ba66fee
vamsinrv <58478414+vamsinrv@users.noreply.github.com>
2019-12-04 16:10:30 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const stats = {};
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    let ip = req.connection.remoteAddress;
+    stats[ip] = stats[ip] ? stats[ip] + 1 : 1;
+    res.send(stats);
+});
+app.listen(8080);
